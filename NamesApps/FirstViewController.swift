64f0651708c632f1e064b7fe725d271fec46412c
Zhan <galymzhan.aitkaliyev@gmail.com>
2023-03-28 19:20:08 +0600

    Initial Commit

---
+++ NamesApps/FirstViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  NamesApps
+//
+//  Created by Perizat Omar on 28.01.2023.
+//
+
+import UIKit
+
+class  FirstViewController: UIViewController {
+
+    @IBOutlet weak var yourNameTF: UITextField!
+    
+    @IBOutlet weak var partnerNameTF: UITextField!
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let destinationVC = segue.destination as? ResultViewController else { return }
+        destinationVC.firstName = yourNameTF.text
+        destinationVC.secondName = partnerNameTF.text
+        
+        
+    }
+
+    @IBAction func resoultButtonTapped() {
+        guard let firstName = yourNameTF.text, let secondName = partnerNameTF.text else { return }
+        if firstName.isEmpty || secondName.isEmpty {
+            showAlert(
+                title: "Names are missing",
+                message: "Please, enter both names! 😉"
+            )
+            return
+        }
+        performSegue(withIdentifier: "goToResault", sender: nil)
+    }
+    
+    @IBAction func unwindSegueToFirstVC(segue:UIStoryboardSegue){
+//        guard segue.identifier == "unwindSegue" else { return }
+        
+        yourNameTF.text = ""
+        partnerNameTF.text = ""
+        
+    }
+    
+}
+extension FirstViewController {
+    private func showAlert(title: String, message:String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert , animated: true)
+        
+    }
+}
+extension FirstViewController: UITextFieldDelegate {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == yourNameTF {
+            partnerNameTF.becomeFirstResponder()
+        } else {
+            resoultButtonTapped()
+        }
+        return true
+    }
+}
+
+
---
+++ NamesApps/ResultViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ResaultViewController.swift
+//  NamesApps
+//
+//  Created by Perizat Omar on 30.01.2023.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    @IBOutlet weak var namesLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    @IBOutlet weak var progressView: UIProgressView!
+    var firstName: String!
+    var secondName: String!
+    
+    private var resultValue = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        resultValue = findResault()
+        
+        namesLabel.text = "\(firstName ?? "") and \(secondName ?? "")"
+        resultLabel.text = resultValue.formatted(.percent)
+        
+        progressView.progress = Float(resultValue)/100
+        progressView.transform = progressView.transform.scaledBy(x: 1, y: 4 ) 
+    }
+    
+//    @IBAction func backButtonTapped() {
+//        performSegue(withIdentifier: "unwindSegue", sender: nil)
+//    }
+    
+    private func findValue(for name: String) ->Int {
+        var count = 0
+        
+        for character in name.lowercased() {
+            switch character {
+            case "b", "c", "d", "e", "f", "g":
+                count += 1
+            case "j", "k", "l", "m", "n", "o", "p":
+                count += 2
+            default:
+                count += 0
+            }
+        }
+    return count
+    }
+    
+    private func findResault() -> Int {
+        var resault = 0
+        
+        let firstValue = findValue(for: firstName)
+        let secondValue = findValue(for: secondName)
+        let absDifference = abs(firstValue - secondValue)
+        
+        switch absDifference {
+        case 0,1,2:
+            resault = 100
+        case 3,4:
+            resault = 65
+        case 5:
+            resault = 50
+        case 6:
+            resault = 40
+        default:
+            resault = 30
+        }
+        return resault
+    }
+}
